28ddc8b2a2bc87d73742d93bdbe697b6bed4d14f
kylemit <Kylemit@gmail.com>
2024-10-14 11:30:36 -0400

    import api token

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
+// Automatically load environment variables from a `.env` file
+import "@std/dotenv/load";
+
+const apiToken = Deno.env.get("API_TOKEN");
+
 function greet(name: string): string {
-    return `Hello, ${name}!`;
+    return `Hello, ${apiToken}s!`;
   }
   
   console.log(greet("world"));
