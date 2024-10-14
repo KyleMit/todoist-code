702aeac4aff7d52aab32dd5c0b3125a7d8fb8643
kylemit <Kylemit@gmail.com>
2024-10-14 11:39:25 -0400

    add TodoistApi

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# todoist-code
+# Todoist Markdown
+
+## Dependencies
+
+* [Deno, the next-generation JavaScript runtime](https://deno.com/)
+* [@std/dotenv - JSR](https://jsr.io/@std/dotenv)
+* [@doist/todoist-api-typescript - npm](https://www.npmjs.com/package/@doist/todoist-api-typescript)
+  * [Doist/todoist-api-typescript: A TypeScript wrapper for the Todoist REST API.](https://github.com/doist/todoist-api-typescript)
+  * [Overview – REST API Reference | Todoist Developer](https://developer.todoist.com/rest/v2#overview)
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 // Automatically load environment variables from a `.env` file
 import "@std/dotenv/load";
+import { TodoistApi } from 'npm:@doist/todoist-api-typescript'
+
 
 const apiToken = Deno.env.get("API_TOKEN");
 
-function greet(name: string): string {
-    return `Hello, ${apiToken}s!`;
-  }
-  
-  console.log(greet("world"));
+if (!apiToken) {
+    throw new Error("API_TOKEN is required in .env file")
+}
+
+const api = new TodoistApi(apiToken)
+
+api.getTasks()
+    .then((tasks) => console.log(tasks))
+    .catch((error) => console.log(error))
+
+
